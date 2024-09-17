71d7e0e287f16c34bad4f53e0c67873259e3a588
Ahnzi <42331481+ahnzi91@users.noreply.github.com>
2024-09-17 11:24:29 +0900

    Check 버튼 클릭 시 Task List에 밑줄 그어지고 Check 아이콘 -> redo 아이콘으로 변경

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 // 2. "+" 버튼을 클릭하면 할 일이 추가된다.
 // 3. 유저가 삭제 버튼을 누르면 할 일이 삭제된다.
 // 4. 유저가 체크 버튼을 누르면 할 일이 끝나면서 밑줄이 간다.
+//   4.1 isChecked가 false에서 true로 바뀌어야 한다.
+//   4.2 isChecked: true -> 할 일이 끝난 것으로 간주하고 밑줄을 보여준다.
+//   4.3 isChecked: false -> 할 일이 안끝난 것으로 간주하고 밑줄 사라진다.
 // 5. 진행 중, 완료 탭을 누르면 탭의 "under-line"이 이동한다.
 // 6. 완료 탭에는 완료된 할 일만, 진행중 탭에는 진행중인 할 일만 노출된다.
 // 7. 전체 탭을 누르면 전체 할 일이 노출된다.
@@ -14,23 +17,56 @@ let taskList = [];
 addBtn.addEventListener("click", addTask);
 
 function addTask() {
-  let taskContent = taskInput.value;
-  taskList.push(taskContent);
+  let task = {
+    id: randomIDGenerate(),
+    taskContent: taskInput.value,
+    isComplete: false,
+  };
+  taskList.push(task);
+  console.log(taskList);
   render();
 }
 
 function render() {
   let resultHTML = "";
   for (let i = 0; i < taskList.length; i++) {
-    resultHTML += `
-      <div class="task">
-        <div>${taskList[i]}</div>
-        <div>
-          <button>Check</button>
-          <button>Delete</button>
+    if (taskList[i].isComplete === true) {
+      resultHTML += `
+        <div class="task">
+          <div class="task-done">${taskList[i].taskContent}</div>
+          <div>
+            <button type="button" onclick="toggleComplete('${taskList[i].id}')"><i class="fas fa-redo"></i></button>
+            <button type="button"><i class="fas fa-trash-alt text-danger"></i></button>
+          </div>
         </div>
-      </div>
-    `;
+      `;
+    } else {
+      resultHTML += `
+        <div class="task">
+          <div>${taskList[i].taskContent}</div>
+          <div>
+            <button type="button" onclick="toggleComplete('${taskList[i].id}')"><i class="fas fa-check text-success"></i></button>
+            <button type="button"><i class="fas fa-trash-alt text-danger"></i></button>
+          </div>
+        </div>
+      `;
+    }
   }
   document.getElementById("task-board").innerHTML = resultHTML;
 }
+
+// 내가 체크한 taskList의 id와 실제로 taskList 배열에 있는 id 비교하는 함수
+function toggleComplete(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id === id) {
+      taskList[i].isComplete = !taskList[i].isComplete;
+      break;
+    }
+  }
+  render();
+}
+
+function randomIDGenerate() {
+  return "_" + Math.random().toString(36).substr(2, 9);
+}
+// <i class="fas fa-redo"></i>
